e57aa84a8803af40d290e9f0ead50f53ecbc35e1
SaileshBK <sailesh.bk1@icloud.com>
2022-04-17 01:15:07 -0500

    Finished project

--- MachineLearning.py
+++ MachineLearning.py
@@ -2,8 +2,11 @@ from statistics import mode
 import pandas as pnd
 #from sklearn.metrics import accuracy_score
 #from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
+#from sklearn.tree import DecisionTreeClassifier
 import joblib
+
+#visualazing the tree model
+#from sklearn import tree
 
 
 # movies_data = pnd.read_csv('movies.csv')
@@ -30,8 +33,18 @@ import joblib
 
 # now loading the model
 model = joblib.load('movie-type.joblib')
+#example, Age 21 female
 predictions = model.predict([[21,1]])
 print(predictions)
 
 
 
+# for visualazing the tree model
+# movies_data = pnd.read_csv('movies.csv')
+# X = movies_data.drop(columns=['type'])  
+# y = movies_data['type']
+# model = DecisionTreeClassifier()
+# model.fit(X,y)
+
+# tree.export_graphviz(model, out_file='movies-type.dot',feature_names=['age','gender'],class_names=sorted(y.unique()),
+# label='all',rounded=True,filled=True)
