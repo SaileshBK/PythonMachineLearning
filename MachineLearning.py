54518a266df5441281f176c2b5678e14eabf4abc
SaileshBK <sailesh.bk1@icloud.com>
2022-04-17 00:46:20 -0500

    done

--- MachineLearning.py
+++ MachineLearning.py
@@ -1,3 +1,37 @@
+from statistics import mode
 import pandas as pnd
-movies_data = pnd.read_csv('movies.csv')
-movies_data
+#from sklearn.metrics import accuracy_score
+#from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+import joblib
+
+
+# movies_data = pnd.read_csv('movies.csv')
+# X = movies_data.drop(columns=['type'])  
+# y = movies_data['type']
+
+# testing accuracy of the model
+
+# #X_train, X_test, y_train, y_test = train_test_split(X,y,test_size = 0.2)
+# #model = DecisionTreeClassifier()
+# #model.fit(X_train,y_train)
+# #predictions = model.predict(X_test)
+
+# myScore = accuracy_score(y_test,predictions)
+# print(myScore)
+
+# Now, Model persistant
+
+# model = DecisionTreeClassifier()
+# model.fit(X,y)
+#joblib.dump(model,'movie-type.joblib')
+
+
+
+# now loading the model
+model = joblib.load('movie-type.joblib')
+predictions = model.predict([[21,1]])
+print(predictions)
+
+
+
